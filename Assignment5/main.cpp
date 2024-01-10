9e84b187f523eab135c00b54399a00891175d02b
luhexin <1443137434@qq.com>
2024-01-10 11:39:43 +0800

    Assignment5

---
+++ Assignment5/main.cpp
@@ -0,0 +1,44 @@
+#include "Scene.hpp"
+#include "Sphere.hpp"
+#include "Triangle.hpp"
+#include "Light.hpp"
+#include "Renderer.hpp"
+
+// In the main function of the program, we create the scene (create objects and lights)
+// as well as set the options for the render (image width and height, maximum recursion
+// depth, field-of-view, etc.). We then call the render function().
+int main()
+{
+    Scene scene(1280, 960);//设置场景的屏幕尺寸为1280 x 960
+
+    //sph1与sph2是两个球体
+    auto sph1 = std::make_unique<Sphere>(Vector3f(-1, 0, -12), 2);
+    sph1->materialType = DIFFUSE_AND_GLOSSY;//反射类型为漫反射
+    sph1->diffuseColor = Vector3f(0.6, 0.7, 0.8);
+
+    auto sph2 = std::make_unique<Sphere>(Vector3f(0.5, -0.5, -8), 1.5);
+    sph2->ior = 1.5;//反射折射率
+    sph2->materialType = REFLECTION_AND_REFRACTION;//反射+折射
+
+    scene.Add(std::move(sph1));
+    scene.Add(std::move(sph2));
+
+    // 添加若干三角形
+    Vector3f verts[4] = {{-5,-3,-6}, {5,-3,-6}, {5,-3,-16}, {-5,-3,-16}};
+    uint32_t vertIndex[6] = {0, 1, 3, 1, 2, 3};
+    Vector2f st[4] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+    auto mesh = std::make_unique<MeshTriangle>(verts, vertIndex, 2, st);
+    mesh->materialType = DIFFUSE_AND_GLOSSY;//反射类型为漫反射
+
+    scene.Add(std::move(mesh));
+
+    //在场景中加入两个点光源，初始化其点光源的坐标与光线强度
+    scene.Add(std::make_unique<Light>(Vector3f(-20, 70, 20), 0.5));
+    scene.Add(std::make_unique<Light>(Vector3f(30, 50, -12), 0.5));    
+
+    //渲染场景
+    Renderer r;
+    r.Render(scene);
+
+    return 0;
+}
